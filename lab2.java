1285d015d56d3e8c67e13717017588e39279e58a
DylanNeese <neesed@huntington.edu>
2025-09-05 13:25:43 -0400

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -4,9 +4,9 @@
 // calculates primes using Sieve of Eratosthenes
 // demonstrates command line arguments and methods
 // 1. The highest I went to was 999999937, it took a while for me to run it so I decided to stop there. Proceeding would take a lot longer, and could potentially crash my computer.
-//I added another 0 to make it 100000000000 and it wouldnt run at all. 
+//I added another 0 to make it 100000000000 and it wouldnt run at all. In my final test I tried 2,013,265,920 and it originally started to run and then it crashed my visual studio code completley. 
 //2. When you print it has to print out thousands of lines vs not printingits much faster becasue it only does memory and cpu operations.
-//3. Time could be a factor as it takes a lot of time in order to run this program. Also memory could be a factor as it takes a lot of memory to store all the boolean values.
+//3. Time could be a factor as it takes a lot of time in order to run this program. Also memory could be a factor as it takes a lot of memory to store all the boolean values.Language could limit it as well. Virtual machine could be a limit as well.  
 
 public class lab2 {
 
